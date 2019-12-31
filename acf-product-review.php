8efdea4c5ee9dc7c21ed1a9a4cc87890f32704e0
a <a@actzero.ai>
2019-12-30 16:53:24 -0800

    Bump the version

--- acf-product-review.php
+++ acf-product-review.php
@@ -4,7 +4,7 @@
 Plugin Name: Advanced Custom Fields: Product Reviews
 Plugin URI: https://github.com/Inkvi/acf-amazon-image
 Description: ACF Custom Field Type for Product Reviews
-Version: 1.0.2
+Version: 1.0.3
 Author: Alexander Eliseev
 */
 
@@ -34,25 +34,12 @@ class acf_product_review {
 	var $settings;
 	
 	
-	/*
-	*  __construct
-	*
-	*  This function will setup the class functionality
-	*
-	*  @type	function
-	*  @date	17/02/2016
-	*  @since	1.0.0
-	*
-	*  @param	void
-	*  @return	void
-	*/
-	
 	function __construct() {
 		
 		// settings
 		// - these will be passed into the field class.
 		$this->settings = array(
-			'version'	=> '1.0.2',
+			'version'	=> '1.0.3',
 			'url'		=> plugin_dir_url( __FILE__ ),
 			'path'		=> plugin_dir_path( __FILE__ )
 		);
@@ -61,20 +48,6 @@ class acf_product_review {
 		// include field
 		add_action('acf/include_field_types', 	array($this, 'include_field')); // v5
 	}
-	
-	
-	/*
-	*  include_field
-	*
-	*  This function will include the field type class
-	*
-	*  @type	function
-	*  @date	17/02/2016
-	*  @since	1.0.0
-	*
-	*  @param	$version (int) major ACF version. Defaults to false
-	*  @return	void
-	*/
 	
 	function include_field( $version = false ) {
 		
