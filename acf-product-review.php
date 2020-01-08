c758937d9a3be7395602f2b9eeb41414600f5673
a <a@actzero.ai>
2020-01-07 20:32:45 -0800

    Control elementor template shortcodes using repeater field

--- acf-product-review.php
+++ acf-product-review.php
@@ -4,7 +4,7 @@
 Plugin Name: Advanced Custom Fields: Product Reviews
 Plugin URI: https://github.com/Inkvi/acf-amazon-image
 Description: ACF Custom Field Type for Product Reviews
-Version: 1.0.10
+Version: 1.0.11
 Author: Alexander Eliseev
 */
 
@@ -73,7 +73,6 @@ if ( ! class_exists( 'acf_product_review' ) ) :
 			// settings
 			// - these will be passed into the field class.
 			$this->settings = array(
-				'version' => '1.0.6',
 				'url'     => plugin_dir_url( __FILE__ ),
 				'path'    => plugin_dir_path( __FILE__ )
 			);
--- block-templates/roundup-reviews.php
+++ block-templates/roundup-reviews.php
@@ -9,20 +9,30 @@ $description_type = get_field( "description_type" );
 $pros_cons_type   = get_field( "pros_cons_type" );
 $features_type    = get_field( "features_type" );
 
-$fields = array();
-if ($description_type != "none") {
-	array_push($fields, $description_type);
+function get_template_id( $description_type, $pros_cons_type, $features_type ) {
+
+	$fields = array();
+	if ( $description_type != "none" ) {
+		array_push( $fields, $description_type );
+	}
+
+	if ( $pros_cons_type != "none" ) {
+		array_push( $fields, $pros_cons_type );
+	}
+	if ( $features_type != "none" ) {
+		array_push( $fields, $features_type );
+	}
+
+	return implode( "_", $fields );
 }
 
-if ($pros_cons_type != "none") {
-	array_push($fields, $pros_cons_type);
-}
-if ( $features_type != "none" ) {
-	array_push($fields, $features_type);
-}
+$template_id = get_template_id( $description_type, $pros_cons_type, $features_type );
 
 $elementor_templates = get_field( "elementor_templates", 'option' );
-if ( $elementor_templates ) {
-	echo do_shortcode( $elementor_templates[ implode("_", $fields) ] );
+foreach ( $elementor_templates as $template ) {
+
+	if ( $template_id == get_template_id( $template['description_type'], $template['pros_cons_type'], $template['features_type'] ) ) {
+		echo do_shortcode( $template['shortcode'] );
+	}
 }
 ?>
