77428a79d309d7bb26431585eba8bacd08207576
a <a@actzero.ai>
2020-01-07 21:17:30 -0800

    Individual review checkbox that controls review link column

--- acf-product-review.php
+++ acf-product-review.php
@@ -4,7 +4,7 @@
 Plugin Name: Advanced Custom Fields: Product Reviews
 Plugin URI: https://github.com/Inkvi/acf-amazon-image
 Description: ACF Custom Field Type for Product Reviews
-Version: 1.0.11
+Version: 1.0.12
 Author: Alexander Eliseev
 */
 
