8eb9dc400b9596a03487ca9cefe941eb3ce42fe9
Alexander <alex@inkvi.com>
2020-02-14 22:16:25 -0800

    Product Advertising 5.0

--- public/Amazon.php
+++ public/Amazon.php
@@ -1,5 +1,11 @@
 <?php
 
+use Amazon\ProductAdvertisingAPI\v1\ApiException;
+use Amazon\ProductAdvertisingAPI\v1\com\amazon\paapi5\v1\api\DefaultApi;
+use Amazon\ProductAdvertisingAPI\v1\com\amazon\paapi5\v1\GetItemsRequest;
+use Amazon\ProductAdvertisingAPI\v1\com\amazon\paapi5\v1\GetItemsResource;
+use Amazon\ProductAdvertisingAPI\v1\com\amazon\paapi5\v1\PartnerType;
+use Amazon\ProductAdvertisingAPI\v1\Configuration;
 
 class Amazon
 {
@@ -10,6 +16,197 @@ class Amazon
         $tag = $affiliate_settings['associate_id'] ?? '';
 
         return "http://www.amazon.com/dp/" . $asin . "/ref=nosim?tag=" . $tag;
+    }
+
+    static function get_locales()
+    {
+        return apply_filters(__FUNCTION__, array(
+            'US' => __('United States'),
+            'AU' => __('Australia'),
+            'BR' => __('Brazil'),
+            'CA' => __('Canada'),
+            'CN' => __('China'),
+            'FR' => __('France'),
+            'DE' => __('Germany'),
+            'IT' => __('Italy'),
+            'IN' => __('India'),
+            'JP' => __('Japan'),
+            'ES' => __('Spain'),
+            'UK' => __('United Kingdom'),
+        ));
+    }
+
+    static function get_locale_hosts()
+    {
+        return apply_filters(__FUNCTION__, array(
+            'US' => 'webservices.amazon.com',
+            'AU' => 'webservices.amazon.com.au',
+            'BR' => 'webservices.amazon.com.br',
+            'CA' => 'webservices.amazon.ca',
+            'FR' => 'webservices.amazon.fr',
+            'DE' => 'webservices.amazon.de',
+            'IN' => 'webservices.amazon.in',
+            'IT' => 'webservices.amazon.it',
+            'JP' => 'webservices.amazon.co.jp',
+            'MX' => 'webservices.amazon.com.mx',
+            'ES' => 'webservices.amazon.es',
+            'TR' => 'webservices.amazon.com.tr',
+            'AE' => 'webservices.amazon.ae',
+            'UK' => 'webservices.amazon.co.uk',
+        ));
+    }
+
+    static function get_locale_regions()
+    {
+        return apply_filters(__FUNCTION__, array(
+            'US' => 'us-east-1',
+            'AU' => 'us-west-2',
+            'BR' => 'us-east-1',
+            'CA' => 'us-east-1',
+            'FR' => 'eu-west-1',
+            'DE' => 'eu-west-1',
+            'IN' => 'eu-west-1',
+            'IT' => 'eu-west-1',
+            'JP' => 'us-west-2',
+            'MX' => 'us-east-1',
+            'ES' => 'eu-west-1',
+            'TR' => 'eu-west-1',
+            'AE' => 'eu-west-1',
+            'UK' => 'eu-west-1',
+        ));
+    }
+
+    static function get_locale_host($locale)
+    {
+        $locale = self::get_locale($locale);
+        $locale_hosts = self::get_locale_hosts();
+
+        return isset($locale_hosts[$locale]) ? $locale_hosts[$locale] : current($locale_hosts);
+    }
+
+    static function get_locale_region($locale)
+    {
+        $locale = self::get_locale($locale);
+        $locale_regions = self::get_locale_regions();
+
+        return isset($locale_regions[$locale]) ? $locale_regions[$locale] : current($locale_regions);
+    }
+
+
+    static function get_locale($locale)
+    {
+        $locale = strtoupper($locale);
+        $locales = self::get_locales();
+
+        return isset($locales[$locale]) ? $locale : key($locales);
+    }
+
+    static function api_config($locale, $access_key = null, $secret_key = null)
+    {
+        $locale = self::get_locale($locale);
+
+        $affiliate_settings = get_field('amazon_affiliate_settings', 'option');
+        $access_key = $affiliate_settings['access_key'] ?? '';
+        $secret_key = $affiliate_settings['secret_key'] ?? '';
+
+        $config = new Configuration();
+        $config->setAccessKey($access_key);
+        $config->setSecretKey($secret_key);
+        $config->setHost(Amazon::get_locale_host($locale));
+        $config->setRegion(Amazon::get_locale_region($locale));
+
+        return $config;
+    }
+
+    static function parseResponse($browseNodes)
+    {
+        $mappedResponse = array();
+        foreach ($browseNodes as $browseNode) {
+            $mappedResponse[$browseNode->getASIN()] = $browseNode;
+        }
+        return $mappedResponse;
+    }
+
+    static function get_transient_key($identifier, $locale)
+    {
+        return "acf_product_reviews_$identifier}_{$locale}";
+    }
+
+
+    public static function get_images($identifier, $locale = null)
+    {
+        $locale = self::get_locale($locale);
+        $transient = get_transient(self::get_transient_key($identifier, $locale));
+        if ($transient) {
+            return $transient;
+        }
+
+        $identifiers = is_array($identifier) ? array_unique($identifier) : array_unique(array($identifier));
+        $affiliate_settings = get_field('amazon_affiliate_settings', 'option');
+        $associate_tag = $affiliate_settings['associate_id'] ?? '';
+
+        $config = Amazon::api_config($locale);
+        $instance = new DefaultApi(new GuzzleHttp\Client(), $config);
+
+        $resources = array(
+            GetItemsResource::IMAGESPRIMARYSMALL,
+            GetItemsResource::IMAGESPRIMARYMEDIUM,
+            GetItemsResource::IMAGESPRIMARYLARGE,
+            GetItemsResource::IMAGESVARIANTSSMALL,
+            GetItemsResource::IMAGESVARIANTSMEDIUM,
+            GetItemsResource::IMAGESVARIANTSLARGE,
+        );
+
+        $request = new GetItemsRequest();
+        $request->setItemIds($identifiers);
+        $request->setPartnerTag($associate_tag);
+        $request->setPartnerType(PartnerType::ASSOCIATES);
+        $request->setResources($resources);
+
+        try {
+            $getItemsResponse = $instance->getItems($request);
+            if ($getItemsResponse->getItemsResult() != null) {
+                if ($getItemsResponse->getItemsResult()->getItems() != null) {
+                    $responseList = self::parseResponse($getItemsResponse->getItemsResult()->getItems());
+
+                    foreach ($identifiers as $asin) {
+                        $item = $responseList[$asin];
+                        if ($item == null) {
+                            echo "Item not found, check errors", PHP_EOL;
+                            continue;
+                        }
+                        $images = $item->getImages();
+                        if ($images == null) {
+//                            echo "No images found";
+                            continue;
+                        }
+
+                        $primary = $images->getPrimary();
+                        if ($primary == null) {
+//                            echo "No primary image found";
+                            continue;
+                        }
+
+                        $result = [
+                            'large' => $primary->getLarge()->getUrl(),
+                            'medium' => $primary->getMedium()->getUrl(),
+                            'small' => $primary->getSmall()->getUrl()
+                        ];
+                        set_transient(self::get_transient_key($asin, $locale), $result, DAY_IN_SECONDS);
+                        return $result;
+
+                    }
+                }
+            }
+        } catch (ApiException $e) {
+            error_log($e->getMessage());
+        } catch (Exception $e) {
+            error_log($e->getMessage());
+        }
+
+        return [];
+//        $data  = json_decode((string) $response, true);
+//        var_dump($data);
     }
 
 }
--- public/fields/acf-field-product-review.php
+++ public/fields/acf-field-product-review.php
@@ -1,211 +1,219 @@
 <?php
 
 // exit if accessed directly
-if( ! defined( 'ABSPATH' ) ) exit;
+if (!defined('ABSPATH')) exit;
 
 
 // check if class already exists
-if( !class_exists('inkvi_acf_field_asin') ) :
+if (!class_exists('acf_product_reviews_field_asin')) :
 
 
-class inkvi_acf_field_asin extends acf_field {
+    class acf_product_reviews_field_asin extends acf_field
+    {
 
 
-	/*
-	*  __construct
-	*
-	*  This function will setup the field type data
-	*
-	*  @type	function
-	*  @date	5/03/2014
-	*  @since	5.0.0
-	*
-	*  @param	n/a
-	*  @return	n/a
-	*/
+        /*
+        *  __construct
+        *
+        *  This function will setup the field type data
+        *
+        *  @type	function
+        *  @date	5/03/2014
+        *  @since	5.0.0
+        *
+        *  @param	n/a
+        *  @return	n/a
+        */
 
-	function __construct() {
+        function __construct()
+        {
 
-		/*
-		*  name (string) Single word, no spaces. Underscores allowed
-		*/
+            /*
+            *  name (string) Single word, no spaces. Underscores allowed
+            */
 
-		$this->name = 'product_review';
-
-
-		/*
-		*  label (string) Multiple words, can include spaces, visible when selecting a field type
-		*/
-
-		$this->label = __('Product', 'acf-product-review');
+            $this->name = 'product_review';
 
 
-		/*
-		*  category (string) basic | content | choice | relational | jquery | layout | CUSTOM GROUP NAME
-		*/
+            /*
+            *  label (string) Multiple words, can include spaces, visible when selecting a field type
+            */
 
-		$this->category = 'Product Review';
-
-
-		/*
-		*  defaults (array) Array of default settings which are merged into the field object. These are used later in settings
-		*/
-
-		$this->defaults = array(
-            'return_format'	=> 'ASIN',
-            'asin-field'	=> 'asin',
-            'asin-repeater-field'	=> 'reviews',
-		);
+            $this->label = __('Product', 'acf-product-review');
 
 
+            /*
+            *  category (string) basic | content | choice | relational | jquery | layout | CUSTOM GROUP NAME
+            */
 
-		$this->associatesTag= get_field('amazon_affiliate_settings', 'option')['associate_id'] ?? '';
-
-		// do not delete!
-    	parent::__construct();
-
-	}
+            $this->category = 'Product Review';
 
 
-	/*
-	*  render_field_settings()
-	*
-	*  Create extra settings for your field. These are visible when editing a field
-	*
-	*  @type	action
-	*  @since	3.6
-	*  @date	23/01/13
-	*
-	*  @param	$field (array) the $field being edited
-	*  @return	n/a
-	*/
+            /*
+            *  defaults (array) Array of default settings which are merged into the field object. These are used later in settings
+            */
 
-	function render_field_settings( $field ) {
-
-		/*
-		*  acf_render_field_setting
-		*
-		*  This function will create a setting for your field. Simply pass the $field parameter and an array of field settings.
-		*  The array of settings does not require a `value` or `prefix`; These settings are found from the $field array.
-		*
-		*  More than one setting can be added by copy/paste the above code.
-		*  Please note that you must also have a matching $defaults value for the field name (font_size)
-		*/
-
-		acf_render_field_setting( $field, array(
-			'label'			=> __('ASIN','acf-product-review'),
-			'instructions'	=> __('ASIN for an Amazon product','acf-product-review'),
-			'type'			=> 'text',
-			'name'			=> 'asin',
-		));
-
-		acf_render_field_setting( $field, array(
-			'label'			=> __('ASIN Field Name','acf-product-review'),
-			'instructions'	=> __('ACF Field Name containing ASIN','acf-product-review'),
-			'type'			=> 'text',
-			'name'			=> 'asin-field',
-		));
-
-		acf_render_field_setting( $field, array(
-			'label'			=> __('Repeater Field Name','acf-product-review-repeater-name'),
-			'instructions'	=> __('ACF Repeater Field Name','acf-product-review-repeater-name'),
-			'type'			=> 'text',
-			'name'			=> 'asin-repeater-field',
-		));
-
-		acf_render_field_setting( $field, array(
-			'label'			=> __('Return Format','acf-product-review'),
-			'instructions'	=> '',
-			'type'			=> 'radio',
-			'name'			=> 'return_format',
-			'layout'		=> 'horizontal',
-			'choices'		=> array(
-				'ASIN'			=> __("ASIN",'acf-product-review'),
-				'image_html'	=> __("Image HTML",'acf-product-review'),
-				'page_link'		=> __("Page Link",'acf-product-review'),
-				'title' 		=> __("Title",'acf-product-review'),
-				'button' 		=> __("Checkout button",'acf-product-review')
-			)
-		));
-
-	}
+            $this->defaults = array(
+                'return_format' => 'ASIN',
+                'asin-field' => 'asin',
+                'asin-repeater-field' => 'reviews',
+            );
 
 
-    function render_asin_field($field) {
-        $html = '';
-		$input_attrs = array();
-		foreach( array( 'value', 'required', "name" ) as $k ) {
-			if( isset($field[ $k ]) ) {
-				$input_attrs[ $k ] = $field[ $k ];
-			}
-		}
-		$html .= '<div class="acf-input-wrap">' . acf_get_text_input( acf_filter_attrs($input_attrs) ) . '</div>';
-		echo $html;
-    }
+            $this->associatesTag = get_field('amazon_affiliate_settings', 'option')['associate_id'] ?? '';
 
-    public function render_image_field($field) {
-        preg_match('/.*row-(\d+)/', $field['prefix'], $matches);
-        if (sizeof($matches) < 2) {
-            return;
+            // do not delete!
+            parent::__construct();
+
         }
-        $repeater_row_index = $matches[1];
 
-        $field_name = $field['asin-repeater-field']."_".$repeater_row_index."_".$field['asin-field'];
-        $asin = get_field($field_name, false, false);
-        if (!empty($asin)) {
-	        $template = "SmallImage";
-	        echo do_shortcode( "[amazon_link asins='" . $asin . "' template='{$template}' store='".$this->associatesTag."' marketplace='US' width='10px']" );
+
+        /*
+        *  render_field_settings()
+        *
+        *  Create extra settings for your field. These are visible when editing a field
+        *
+        *  @type	action
+        *  @since	3.6
+        *  @date	23/01/13
+        *
+        *  @param	$field (array) the $field being edited
+        *  @return	n/a
+        */
+
+        function render_field_settings($field)
+        {
+
+            /*
+            *  acf_render_field_setting
+            *
+            *  This function will create a setting for your field. Simply pass the $field parameter and an array of field settings.
+            *  The array of settings does not require a `value` or `prefix`; These settings are found from the $field array.
+            *
+            *  More than one setting can be added by copy/paste the above code.
+            *  Please note that you must also have a matching $defaults value for the field name (font_size)
+            */
+
+            acf_render_field_setting($field, array(
+                'label' => __('ASIN', 'acf-product-review'),
+                'instructions' => __('ASIN for an Amazon product', 'acf-product-review'),
+                'type' => 'text',
+                'name' => 'asin',
+            ));
+
+            acf_render_field_setting($field, array(
+                'label' => __('ASIN Field Name', 'acf-product-review'),
+                'instructions' => __('ACF Field Name containing ASIN', 'acf-product-review'),
+                'type' => 'text',
+                'name' => 'asin-field',
+            ));
+
+            acf_render_field_setting($field, array(
+                'label' => __('Repeater Field Name', 'acf-product-review-repeater-name'),
+                'instructions' => __('ACF Repeater Field Name', 'acf-product-review-repeater-name'),
+                'type' => 'text',
+                'name' => 'asin-repeater-field',
+            ));
+
+            acf_render_field_setting($field, array(
+                'label' => __('Return Format', 'acf-product-review'),
+                'instructions' => '',
+                'type' => 'radio',
+                'name' => 'return_format',
+                'layout' => 'horizontal',
+                'choices' => array(
+                    'ASIN' => __("ASIN", 'acf-product-review'),
+                    'image_html' => __("Image HTML", 'acf-product-review'),
+                    'page_link' => __("Page Link", 'acf-product-review'),
+                    'title' => __("Title", 'acf-product-review'),
+                    'button' => __("Checkout button", 'acf-product-review')
+                )
+            ));
+
         }
-    }
-
-    function render_url_field($field) {
-    }
-
-    function render_title_field($field) {
-        return $this->render_asin_field($field);
-    }
 
 
-	function render_field( $field ) {
-		if ($field['return_format']=="ASIN") {
-		    $this->render_asin_field($field);
-		}
+        function render_asin_field($field)
+        {
+            $html = '';
+            $input_attrs = array();
+            foreach (array('value', 'required', "name") as $k) {
+                if (isset($field[$k])) {
+                    $input_attrs[$k] = $field[$k];
+                }
+            }
+            $html .= '<div class="acf-input-wrap">' . acf_get_text_input(acf_filter_attrs($input_attrs)) . '</div>';
+            echo $html;
+        }
 
-		if ($field['return_format']=="image_html") {
-		    $this->render_image_field($field);
-		}
+        public function render_image_field($field)
+        {
+            preg_match('/.*row-(\d+)/', $field['prefix'], $matches);
+            if (sizeof($matches) < 2) {
+                return;
+            }
+            $repeater_row_index = $matches[1];
 
-		if ($field['return_format']=="title") {
-		    $this->render_title_field($field);
-		}
-	}
+            $field_name = $field['asin-repeater-field'] . "_" . $repeater_row_index . "_" . $field['asin-field'];
+            $asin = get_field($field_name, false, false);
+            if (!empty($asin)) {
+                $images = Amazon::get_images($asin);
+                if ($images) {
+                    echo '<img src="' . $images['small'] . '" style="display: block; margin: 0 auto;"/>';
+                }
+            }
+        }
+
+        function render_title_field($field)
+        {
+            return $this->render_asin_field($field);
+        }
 
 
-	function load_value( $value, $post_id, $field ) {
-		return $value;
+        function render_field($field)
+        {
+            if ($field['return_format'] == "ASIN") {
+                $this->render_asin_field($field);
+            }
 
-	}
+            if ($field['return_format'] == "image_html") {
+                $this->render_image_field($field);
+            }
 
-	function format_value( $value, $post_id, $field ) {
+            if ($field['return_format'] == "title") {
+                $this->render_title_field($field);
+            }
+        }
+
+
+        function load_value($value, $post_id, $field)
+        {
+            return $value;
+
+        }
+
+        function format_value($value, $post_id, $field)
+        {
 //		var_dump("format_value", $field['name'], $field['return_format']);
-		if ($field['return_format']=="ASIN") {
-		    return $value;
-		}
+            if ($field['return_format'] == "ASIN") {
+                return $value;
+            }
 
-        preg_match('/(.*_\d+_).*/', $field['name'], $matches);
-        $field_name = $matches[1].$field['asin-field'];
-        $asin = acf_get_metadata($post_id, $field_name);
+            preg_match('/(.*_\d+_).*/', $field['name'], $matches);
+            $field_name = $matches[1] . $field['asin-field'];
+            $asin = acf_get_metadata($post_id, $field_name);
 
-		if ($field['return_format']=="image_html") {
-		    $template = "ImageLink";
-            return do_shortcode("[amazon_link asins='".acf_get_metadata($post_id, $field_name)."' template='"
-                                .$template."' store='".$this->associatesTag."' marketplace='US']");
-		}
+            if ($field['return_format'] == "image_html") {
+                $images = Amazon::get_images($asin);
+                if ($images) {
+                    return '<img src="' . $images['medium'] . '" srcset="' . $images['large'] . '" style="display: block; margin: 0 auto;"/>';
+                }
+            }
 
-		if ($field['return_format']=="title") {
-			$url = Amazon::get_amazon_url($asin);
+            if ($field['return_format'] == "title") {
+                $url = Amazon::get_amazon_url($asin);
 
-$heading=<<<HEADING
+                $heading = <<<HEADING
 <div class="elementor-element elementor-widget elementor-widget-heading" data-element_type="widget" data-widget_type="heading.default">
     <div class="elementor-widget-container">
         <h2 class="elementor-heading-title elementor-size-default">
@@ -214,23 +222,16 @@ $heading=<<<HEADING
     </div>
 </div>
 HEADING;
-            return $heading;
-		}
-
-		if ($field['return_format']=="button") {
-            preg_match('/(.*_\d+_).*/', $field['name'], $matches);
-            $field_name = $matches[1].$field['asin-field'];
-            $url = do_shortcode("[amazon_link asins='".acf_get_metadata($post_id, $field_name)."' template='DetailPageLink' store='".$this->associatesTag."' marketplace='US']");
-
-            # amazon plugin inserts an img tag as a pixel
-            preg_match('/<img.*>(.*)/', $url, $matches);
-            if (count($matches) > 0) {
-                $url = trim($matches[1]);
+                return $heading;
             }
 
-            $cta_text = get_field('amazon_affiliate_settings', 'option')['cta_text'] ?? 'Show Me Price';;
+            if ($field['return_format'] == "button") {
+                preg_match('/(.*_\d+_).*/', $field['name'], $matches);
+                $field_name = $matches[1] . $field['asin-field'];
+                $url = Amazon::get_amazon_url(acf_get_metadata($post_id, $field_name));
+                $cta_text = get_field('amazon_affiliate_settings', 'option')['cta_text'] ?? 'Show Me Price';;
 
-$value = <<<EOL
+                $value = <<<EOL
 <div class="elementor-element elementor-button-danger elementor-align-center elementor-widget elementor-widget-button" data-element_type="widget" data-widget_type="button.default">
     <div class="elementor-widget-container">
         <div class="elementor-button-wrapper">
@@ -243,19 +244,17 @@ $value = <<<EOL
     </div>
 </div>
 EOL;
-            return $value;
+                return $value;
+            }
+
+
         }
+    }
 
 
-	}
-}
+    new acf_product_reviews_field_asin();
 
 
-// initialize
-new inkvi_acf_field_asin();
-
-
-// class_exists check
 endif;
 
 ?>
