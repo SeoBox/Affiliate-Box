428a932b6d4db6919281dc6d6ad52fb2f88a451e
a <a@actzero.ai>
2020-04-26 17:41:49 -0700

    Added unit tests to cover cons and pros

--- tests/BlocksToProductConverterTest.php
+++ tests/BlocksToProductConverterTest.php
@@ -13,12 +13,14 @@ class BlocksToProductConverterTest extends TestCase
         $asin = "B01AJJIQQQ";
         $title = "Rockjam Piano";
         $bestCategory = "Best Piano Keyboard for Kids Overall";
-        $description = "<p>The RockJam keyboard has 61 keys</p>";
+        $description = "<p><span>The RockJam keyboard has 61 keys</span></p>";
+        $pro = "Great teaching tool.";
+        $con = "The software is complicated for kids.";
         $documents = array(
             array(
                 array(
                     "blockName" => "core/heading",
-                    "innerHTML" => "<h2><span><a href=\"https://www.amazon.com/dp/B01AJJIQQQ\">$title</a></span><span>&nbsp;— $bestCategory</span></h2>"
+                    "innerHTML" => "<h3><span><a href=\"https://www.amazon.com/dp/B01AJJIQQQ\">$title</a></span><span>&nbsp;— $bestCategory</span></h3>"
                 ),
                 array(
                     "blockName" => "core/paragraph",
@@ -26,11 +28,11 @@ class BlocksToProductConverterTest extends TestCase
                 ),
                 array(
                     "blockName" => "core/heading",
-                    "innerHTML" => "<h3><span>Pros</span></h3>"
+                    "innerHTML" => "<h4><span>Pros</span></h4>"
                 ),
                 array(
                     "blockName" => "core/list",
-                    "innerHTML" => "<ul><li><span>Great teaching tool.</span></li></ul>"
+                    "innerHTML" => "<ul><li><span>$pro</span></li></ul>"
                 ),
                 array(
                     "blockName" => "core/paragraph",
@@ -38,7 +40,7 @@ class BlocksToProductConverterTest extends TestCase
                 ),
                 array(
                     "blockName" => "core/list",
-                    "innerHTML" => "<ul><li><span>The software is complicated for kids.</span></li></ul>"
+                    "innerHTML" => "<ul><li><span>$con</span></li></ul>"
                 )
             ),
             array(
@@ -56,7 +58,7 @@ class BlocksToProductConverterTest extends TestCase
                 ),
                 array(
                     "blockName" => "core/list",
-                    "innerHTML" => "<ul><li><span>Great teaching tool.</span></li></ul>"
+                    "innerHTML" => "<ul><li><span>$pro</span></li></ul>"
                 ),
                 array(
                     "blockName" => "core/paragraph",
@@ -64,7 +66,7 @@ class BlocksToProductConverterTest extends TestCase
                 ),
                 array(
                     "blockName" => "core/list",
-                    "innerHTML" => "<ul><li><span>The software is complicated for kids.</span></li></ul>"
+                    "innerHTML" => "<ul><li><span>$con</span></li></ul>"
                 ),
                 array(
                     "blockName" => "core/heading",
@@ -87,15 +89,15 @@ class BlocksToProductConverterTest extends TestCase
                 ),
                 array(
                     "blockName" => "core/list",
-                    "innerHTML" => "<ul><li><span>Great teaching tool.</span></li></ul>"
+                    "innerHTML" => "<ul><li><span>$pro</span></li></ul>"
                 ),
                 array(
                     "blockName" => "core/paragraph",
-                    "innerHTML" => "<p><span>Cons</span></p>"
+                    "innerHTML" => "<h4><span>What We Don't Like</span></h4>"
                 ),
                 array(
                     "blockName" => "core/list",
-                    "innerHTML" => "<ul><li><span>The software is complicated for kids.</span></li></ul>"
+                    "innerHTML" => "<ul><li><span>$con</span></li></ul>"
                 ),
                 array(
                     "blockName" => "core/heading",
@@ -109,13 +111,13 @@ class BlocksToProductConverterTest extends TestCase
         );
         $allExpectedProducts = array(
             array(
-                array("asin" => $asin, "title" => $title, "bestCategory" => $bestCategory, "description" => $description)
+                array("asin" => $asin, "title" => $title, "bestCategory" => $bestCategory, "description" => $description, 'pros' => array($pro), 'cons' => array($con))
             ),
             array(
-                array("asin" => $asin, "title" => $title, "bestCategory" => $bestCategory, "description" => $description)
+                array("asin" => $asin, "title" => $title, "bestCategory" => $bestCategory, "description" => $description, 'pros' => array($pro), 'cons' => array($con))
             ),
             array(
-                array("asin" => $asin, "title" => $title, "bestCategory" => $bestCategory, "description" => $description)
+                array("asin" => $asin, "title" => $title, "bestCategory" => $bestCategory, "description" => $description, 'pros' => array($pro), 'cons' => array($con))
             ),
         );
 
@@ -126,6 +128,7 @@ class BlocksToProductConverterTest extends TestCase
                 self::assertEquals($product->title, $expectedProduct['title']);
                 self::assertEquals($product->bestCategory, $expectedProduct['bestCategory']);
                 self::assertEquals($product->description, $expectedProduct['description']);
+                self::assertEquals($product->pros, $expectedProduct['pros']);
             }
         }
     }
