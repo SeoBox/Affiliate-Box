7356681b052a1ebd95108fa24d0f27a7ab63db94
a <a@actzero.ai>
2020-06-07 16:47:44 -0700

    Include acffa in the compressed file

--- compress.sh
+++ compress.sh
@@ -1,1 +1,1 @@
-zip -r affiliate-box.zip plugin-update-checker readme.txt admin includes public vendor affiliate-box.php index.php uninstall.php
+zip -r affiliate-box.zip acffa plugin-update-checker readme.txt admin includes public vendor affiliate-box.php index.php uninstall.php
